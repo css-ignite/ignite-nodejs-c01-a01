c15b296b1d71ec4dd84116f0ad9e6ffd550e9f7c
claudneysessa <claudneysartisessa@gmail.com>
2022-10-23 16:39:56 -0300

    Configurando as rotas no express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 // Criando uma constante com a instância do express
-const express = require('express');
+const express = require("express");
 
 // Instanciando o express
 const app = express();
+
+// Criando uma rota de Get
+app.get("/", (req, res) => {
+  return res.send("Hello World Ignite - Fundamentos NodeJS");
+});
+
+// Criando uma rota de Get
+app.get("/json", (req, res) => {
+  return res.json({
+    message: "Hello World Ignite - Fundamentos NodeJS",
+  });
+});
 
 // Adicionando um listen para execução do express na porta 3333
 app.listen(3333);
